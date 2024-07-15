7d2cc851da39c5812e0c48897c1cbf8001371897
Anton <anton.francois134@gmail.com>
2024-07-15 11:54:08 +0200

    add simplex + experiments

--- demeter/metamorphosis/simplex.py
+++ demeter/metamorphosis/simplex.py
@@ -137,8 +137,6 @@ class Simplex_sqrt_Metamorphosis_integrator(Geodesic_integrator):
                 self.field_stock[i] = field_to_stock[0].detach().to('cpu')
                 self.momentum_stock[i] = momentum_dt[0].detach().to('cpu')
                 self.residuals_stock[i] = residuals_dt.detach().to('cpu')
-            # elif self.save is False and i == 0:
-            #     self.momentum_ini = residuals_dt.detach().to('cpu')
 
             if verbose:
                 update_progress(i/(t_max*self.n_step))
@@ -178,15 +176,6 @@ class Simplex_sqrt_Shooting(Optimize_geodesicShooting):
         self.data_loss = self.data_term()
 
         # Norm V
-        # self.norm_v_2 = self._compute_V_norm_(momentum_ini,self.source)
-        #
-        # # norm_2 on z
-        # # # Norm on the residuals only
-        # # self.norm_l2_on_z = (momentum_ini**2).sum()/prod(self.source.shape[2:])
-        # self.norm_l2_on_z = (self.mp.residual_ini**2).sum()/prod(self.source.shape[2:])
-        #
-        # self.total_cost = self.data_loss + \
-        #                   self.cost_cst * .5 * ( rho * self.norm_v_2 + (1 - rho) * self.norm_l2_on_z)
 
         # TODO: explain why there is a rho here
         self.norm_v_2 = rho  * self._compute_V_norm_(momentum_ini,self.source)
