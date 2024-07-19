d16783348bfa4ae0162f336fbeca5994ddd8dfda
Anton <anton.francois134@gmail.com>
2024-07-19 14:27:35 +0200

    modify signature of get_deformat(or,ion) to be able to start from whenever I want.

--- demeter/metamorphosis/abstract.py
+++ demeter/metamorphosis/abstract.py
@@ -322,33 +322,61 @@ class Geodesic_integrator(torch.nn.Module,ABC):
         self.field = tb.im2grid(self.kernelOperator(tb.grid2im(free_field + oriented_field)))
 
 
-    def get_deformation(self,n_step=None,save=False):
+    def get_deformation(self,from_t=0,to_t=None,save=False):
         r"""Returns the deformation use it for showing results
-        $\Phi = \int_0^1 v_t dt$
+        $\Phi = \int_s^t v_t dt$ with $s < t$ and $t \in [0,n_{step}-1]$
 
-        :return: deformation [1,H,W,2] or [2,H,W,D,3]
-        """
-        if n_step == 0:
-            return self.id_grid.detach().cpu() + self.field_stock[0][None].detach().cpu()/self.n_step
-        temporal_integrator = vff.FieldIntegrator(method='temporal',save=save)
-        if n_step is None:
-            return temporal_integrator(self.field_stock/self.n_step,forward=True)
-        else:
-            return temporal_integrator(self.field_stock[:n_step]/self.n_step,forward=True)
-
-    def get_deformator(self,n_step=None,save = False):
-        r"""Returns the inverse deformation use it for deforming images
-        $\Phi^{-1}$
-
+        :params: from_t : (int) the starting time step (default 0)
+        :params: to_t : (int) the ending time step (default n_step)
+        :params: save : (bool) option to save the integration intermediary steps. If true, the return value will have its shape with T>1
         :return: deformation [T,H,W,2] or [T,H,W,D,3]
         """
-        if n_step == 0:
-            return self.id_grid.detach().cpu()-self.field_stock[0][None].detach().cpu()/self.n_step
+        # if n_step == 0:
+        #     return self.id_grid.detach().cpu() + self.field_stock[0][None].detach().cpu()/self.n_step
+        # temporal_integrator = vff.FieldIntegrator(method='temporal',save=save)
+        # if n_step is None:
+        #     return temporal_integrator(self.field_stock/self.n_step,forward=True)
+        # else:
+        #     return temporal_integrator(self.field_stock[:n_step]/self.n_step,forward=True)
+        #
         temporal_integrator = vff.FieldIntegrator(method='temporal',save=save)
-        if n_step is None:
-            return temporal_integrator(self.field_stock/self.n_step,forward=False)
-        else:
-            return temporal_integrator(self.field_stock[:n_step]/self.n_step,forward=False)
+        if from_t is None and to_t is None:
+            print('Je suis passé par là')
+            return temporal_integrator(self.field_stock/self.n_step, forward=False)
+        # if from_t is None: from_t = 0
+        if to_t is None: to_t = self.n_step
+        if from_t < 0 and from_t >= to_t:
+            raise ValueError(f"from_t must in [0,n_step-1], got from_t ={from_t} and n_step = {self.n_step}")
+        if to_t > self.n_step:
+            raise ValueError(f"to_t must in [from_t+1,n_step], got to_t ={to_t} and n_step = {self.n_step}")
+        if to_t==1:
+            return self.id_grid.detach().cpu() + self.field_stock[0][None].detach().cpu() / self.n_step
+        # ic(from_t,to_t,self.field_stock[from_t:to_t].shape)
+        return temporal_integrator(self.field_stock[from_t:to_t]/self.n_step, forward=True)
+
+    def get_deformator(self,from_t=0,to_t=None,save=False):
+        r"""Returns the inverse deformation use it for deforming images
+        $(\Phi_{s,t})^{-1}$ with $s < t$ and $t \in [0,n_{step}-1]$
+
+        :params: from_t : (int) the starting time step (default 0)
+        :params: to_t : (int) the ending time step (default n_step)
+        :params: save : (bool) option to save the integration intermediary steps. If true, the return value will have its shape with T>1
+        :return: deformation [T,H,W,2] or [T,H,W,D,3]
+        """
+        temporal_integrator = vff.FieldIntegrator(method='temporal',save=save)
+        if from_t is None and to_t is None:
+            print('Je suis passé par là')
+            return temporal_integrator(self.field_stock/self.n_step, forward=False)
+        # if from_t is None: from_t = 0
+        if to_t is None: to_t = self.n_step
+        if from_t < 0 and from_t >= to_t:
+            raise ValueError(f"from_t must in [0,n_step-1], got from_t ={from_t} and n_step = {self.n_step}")
+        if to_t > self.n_step:
+            raise ValueError(f"to_t must in [from_t+1,n_step], got to_t ={to_t} and n_step = {self.n_step}")
+        if to_t==1:
+            return self.id_grid.detach().cpu() - self.field_stock[0][None].detach().cpu() / self.n_step
+        # ic(from_t,to_t,self.field_stock[from_t:to_t].shape)
+        return temporal_integrator(self.field_stock[from_t:to_t]/self.n_step, forward=False)
 
     # ==================================================================
     #                       PLOTS
