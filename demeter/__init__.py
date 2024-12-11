cbda2ade6a9d0d5597f4ca64db0d830238a144fb
antonfrancois <anton.francois134@gmail.com>
2024-12-11 16:48:04 +0100

    to get icecream debug globally

--- demeter/__init__.py
+++ demeter/__init__.py
@@ -1,2 +1,9 @@
 # import metamorphosis
 # import utils
+
+try:
+    from icecream import install,ic
+    install()
+    ic.configureOutput(includeContext=True)
+except ImportError:  # Graceful fallback if IceCream isn't installed.
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
