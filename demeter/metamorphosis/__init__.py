d379ecd799cd65d3e77221321e5f26e40a26b736
antonfrancois <anton.francois134@gmail.com>
2025-01-19 18:04:54 +0100

    import fill_saves_overview.py in metamorphosis namespace

--- demeter/metamorphosis/__init__.py
+++ demeter/metamorphosis/__init__.py
@@ -3,6 +3,7 @@ from .classic import Metamorphosis_integrator, Metamorphosis_Shooting
 from .constrained import *
 from .wraps import *
 from .load import load_optimize_geodesicShooting
+from ..utils.fill_saves_overview import *
 from .joined import Weighted_joinedMask_Metamorphosis_integrator,Weighted_joinedMask_Metamorphosis_Shooting
 from .simplex import Simplex_sqrt_Metamorphosis_integrator,Simplex_sqrt_Shooting
 from .data_cost import *
