59d36f39e281fa373bb7e5dda1ad64639c12361e
antonfrancois <anton.francois134@gmail.com>
2025-01-27 19:57:58 +0100

    to check whats inside ROOT_DIRECTORY

--- test/test_constant.py
+++ test/test_constant.py
@@ -9,3 +9,13 @@ def test_constant():
     assert isinstance(dm.ROOT_DIRECTORY, str)
     assert isinstance(dm.OPTIM_SAVE_DIR, str)
     assert isinstance(dm.FIELD_TO_SAVE, list)
+
+def test_at_build():
+    print(f"ROOT DIRECTORY: {dm.ROOT_DIRECTORY}")
+
+    print(f"in ROOT_DIRECTORY: {os.listdir(dm.ROOT_DIRECTORY)}")
+    print(f"in .. ROOT_DIRECTORY: {os.listdir(os.path.join(dm.ROOT_DIRECTORY,'..'))}")
+    assert os.path.exists(dm.ROOT_DIRECTORY)
+    assert os.path.exists(dm.OPTIM_SAVE_DIR)
+    assert os.path.exists(dm.ROOT_DIRECTORY+"/examples/")
+
