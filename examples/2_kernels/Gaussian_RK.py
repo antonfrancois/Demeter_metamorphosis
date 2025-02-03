a035434e902795622c56486824c028ddb61cb5ec
antonfrancois <anton.francois134@gmail.com>
2025-02-03 13:55:54 +0100

    add Gaussian_RK.py

---
+++ examples/2_kernels/Gaussian_RK.py
@@ -0,0 +1,18 @@
+"""
+.. _gaussianRK:
+
+A multi-scale Gaussian Reproducing Kernel
+================================================
+"""
+
+######################################################################
+# Importing the necessary libraries
+import numpy as np
+
+from demeter.constants import DLT_KW_IMAGE
+import matplotlib.pyplot as plt
+import torch
+import demeter.utils.reproducing_kernels as rk
+import demeter.utils.torchbox as tb
+
+######################################################################
